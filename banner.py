195f5ce3b23ea0cc568a91cbcdf77658c670a96b
Brad Follett <Bradley.Follett1@gmail.com>
2019-09-10 14:17:03 -0400

    Adds conditional code to banner so we can import it

--- banner.py
+++ banner.py
@@ -11,10 +11,10 @@ def banner(title , author):
     print("-" * banner_length)
     print("")
 
+if __name__ == "__main__":
+    banner("BANNER" , "Karl Marx")
 
-banner("BANNER" , "Karl Marx")
-
-name = input("What is your name?")
-quest = input("What is the quest?")
-print("")
-banner(quest , name)
+    name = input("What is your name?")
+    quest = input("What is the quest?")
+    print("")
+    banner(quest , name)
